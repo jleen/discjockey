8501567e0fd9f631c43b128b9e89f2f6a705d447
John Leen <jleen@saturnvalley.org>
2012-05-15 23:00:15 -0700

    Dry run for playlist munging

--- dj.py
+++ dj.py
@@ -18,6 +18,13 @@ def zeroes(num): return '0' * int(1 + math.floor(math.log10(num)))
 def munge_m3u(rel_dir, file):
     # TODO(jleen): Hard link if no changes needed.
     print 'Munging playlist %s in %s' % (file, rel_dir)
+    f = open(os.path.join(args.music, rel_dir), 'r')
+    for line in f:
+        ext = os.path.splitext(line)[1].lower()
+        if ext in transcode_formats:
+            print '   Adding %s' % (transcoded_filename(line))
+        else:
+            print '   Passing through %s' % (line)
 
 def create_m3u(rel_dir, files):
     music_files = [x for x in files if os.path.splitext(x)[1].lower() in
@@ -37,7 +44,7 @@ def create_link(rel_dir, file):
 
 for dir, dirs, files in os.walk(args.music):
     assert dir[0:len(args.music)] == args.music
-    rel_dir = dir[len(args.music):]
+    rel_dir = dir[1 + len(args.music):]
     did_music = False; did_playlist = False
     for file in files:
         ext = os.path.splitext(file)[1].lower()
