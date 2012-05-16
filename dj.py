2dc1ea73bb8a4efce7039fc67a67b29792192355
John Leen <jleen@saturnvalley.org>
2012-05-16 00:31:17 -0700

    Munge transcoded filenames in created playlists.  Logging.

--- dj.py
+++ dj.py
@@ -1,45 +1,56 @@
 # Copyright (c) 2012 John Leen
 
 import argparse
+import logging
 import math
 import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--music', metavar='DIR')
 parser.add_argument('--cache', metavar='DIR')
+parser.add_argument('-v', '--verbose', action='store_true')
 
 args = parser.parse_args()
+if args.verbose: logging.basicConfig(level=logging.INFO)
 
 transcode_formats = [ '.flac' ]
 okay_formats = [ '.mp3', '.ogg' ]
 
+def base(filename): return os.path.splitext(filename)[0]
+def extension(filename): return os.path.splitext(filename)[1].lower()
 def zeroes(num): return '0' * int(1 + math.floor(math.log10(num)))
+
+def transcoded_filename(filename):
+    return base(filename) + '.ogg'
 
 def munge_m3u(rel_dir, file):
     # TODO(jleen): Hard link if no changes needed.
-    print 'Munging playlist %s in %s' % (file, rel_dir)
-    f = open(os.path.join(args.music, rel_dir), 'r')
+    logging.info('Munging playlist %s in %s' % (file, rel_dir))
+    f = open(os.path.join(args.music, rel_dir, file), 'r')
     for line in f:
-        ext = os.path.splitext(line)[1].lower()
-        if ext in transcode_formats:
-            print '   Adding %s' % (transcoded_filename(line))
+        line = line.rstrip()
+        if extension(line) in transcode_formats:
+            logging.info('   Adding %s' % (transcoded_filename(line)))
         else:
-            print '   Passing through %s' % (line)
+            logging.info('   Passing through %s' % (line))
 
 def create_m3u(rel_dir, files):
-    music_files = [x for x in files if os.path.splitext(x)[1].lower() in
+    music_files = [x for x in files if extension(x) in
             okay_formats + transcode_formats]
     m3u_filename = '%s %s.m3u' % (zeroes(len(music_files)),
                                   os.path.basename(rel_dir))
-    print 'Creating playlist %s in %s' % (m3u_filename, rel_dir)
-    for music_file in music_files: print '   Adding %s' % (music_file)
+    logging.info('Creating playlist %s in %s' % (m3u_filename, rel_dir))
+    for music_file in music_files:
+        if extension(music_file) in transcode_formats:
+            music_file = transcoded_filename(music_file)
+        logging.info('   Adding %s' % (music_file))
 
 def transcode_flac(rel_dir, file):
     # TODO(jleen): We'll probably have to shell out for this.
-    print 'Transcoding %s in %s' % (file, rel_dir)
+    logging.info('Transcoding %s in %s' % (file, rel_dir))
 
 def create_link(rel_dir, file):
-    print 'Linking %s in %s' % (file, rel_dir)
+    logging.info('Linking %s in %s' % (file, rel_dir))
 
 
 for dir, dirs, files in os.walk(args.music):
@@ -47,7 +58,7 @@ for dir, dirs, files in os.walk(args.music):
     rel_dir = dir[1 + len(args.music):]
     did_music = False; did_playlist = False
     for file in files:
-        ext = os.path.splitext(file)[1].lower()
+        ext = extension(file)
         if ext == '.m3u': munge_m3u(rel_dir, file); did_playlist = True
         if ext == '.flac': transcode_flac(rel_dir, file); did_music = True
         if ext in okay_formats: create_link(rel_dir, file); did_music = True
