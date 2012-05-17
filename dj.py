4c8883e392a5cffa683e5cadb97892ae41310064
John Leen <jleen@saturnvalley.org>
2012-05-16 22:39:33 -0700

    Transcodingls

--- dj.py
+++ dj.py
@@ -4,12 +4,15 @@ import argparse
 import logging
 import math
 import os
-
-from os.path import join
+import subprocess
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--music', metavar='DIR')
 parser.add_argument('--cache', metavar='DIR')
+parser.add_argument('--ogg_bin', metavar='PATH',
+                    default='/opt/local/bin/oggenc')
+parser.add_argument('--flac_bin', metavar='PATH',
+                    default='/opt/local/bin/flac')
 parser.add_argument('-v', '--verbose', action='store_true')
 
 args = parser.parse_args()
@@ -17,6 +20,9 @@ if args.verbose: logging.basicConfig(level=logging.INFO)
 
 transcode_formats = [ '.flac' ]
 okay_formats = [ '.mp3', '.ogg' ]
+
+def music_path(*pathcomps): return os.path.join(args.music, *pathcomps)
+def cache_path(*pathcomps): return os.path.join(args.cache, *pathcomps)
 
 def base(filename): return os.path.splitext(filename)[0]
 def extension(filename): return os.path.splitext(filename)[1].lower()
@@ -30,12 +36,12 @@ def transcoded_filename(filename):
 
 def munge_m3u(rel_dir, file):
     logging.info('Munging playlist %s in %s' % (file, rel_dir))
-    with open(os.path.join(args.music, rel_dir, file), 'r') as f:
+    with open(music_path(rel_dir, file), 'r') as f:
         lines = [x.rstrip() for x in f.readlines()]
 
     if any(extension(line) in transcode_formats for line in lines):
-        ensure_dir(join(args.cache, rel_dir))
-        with open(join(args.cache, rel_dir, file), 'w') as out_f:
+        ensure_dir(cache_path(rel_dir))
+        with open(cache_path(rel_dir, file), 'w') as out_f:
             for line in lines:
                 if extension(line) in transcode_formats:
                     logging.info('   Munging %s' % (transcoded_filename(line)))
@@ -52,8 +58,8 @@ def create_m3u(rel_dir, files):
                                   os.path.basename(rel_dir))
     logging.info('Creating playlist %s in %s' % (m3u_filename, rel_dir))
 
-    ensure_dir(join(args.cache, rel_dir))
-    with open(join(args.cache, rel_dir, m3u_filename), 'w') as out_f:
+    ensure_dir(cache_path(rel_dir))
+    with open(cache_path(rel_dir, m3u_filename), 'w') as out_f:
         for music_file in music_files:
             if extension(music_file) in transcode_formats:
                 music_file = transcoded_filename(music_file)
@@ -61,18 +67,31 @@ def create_m3u(rel_dir, files):
             out_f.write('%s\n' % (music_file))
 
 def transcode_flac(rel_dir, file):
-    # TODO(jleen): We'll probably have to shell out for this.
     logging.info('Transcoding %s in %s' % (file, rel_dir))
-    ensure_dir(join(args.cache, rel_dir))
-    with open(join(args.cache, rel_dir, transcoded_filename(file)),
-              'w') as out_f:
-        out_f.write('This is a transcode of %s' %
-                    (join(args.music, rel_dir, file)))
+    ensure_dir(cache_path(rel_dir))
+    flac_path = music_path(rel_dir, file)
+    ogg_path = cache_path(rel_dir, transcoded_filename(file))
+
+    with open(os.devnull, 'w') as dev_null:
+        decode_proc = subprocess.Popen([args.flac_bin, '-d', '-c', flac_path],
+                                       stdout=subprocess.PIPE, stderr=dev_null)
+        encode_proc = subprocess.Popen([args.ogg_bin, '-', '-o', ogg_path],
+                                       stdin=decode_proc.stdout,
+                                       stdout=subprocess.PIPE, stderr=dev_null)
+        decode_proc.stdout.close()
+        encode_proc.communicate()
+        # TODO(jleen): Is poll() exactly what we want? Is there a race here if
+        # flac sits around after oggenc terminates?
+        decode_proc.poll()
+    if encode_proc.returncode != 0:
+        raise Exception('Abnormal oggenc termination')
+    if decode_proc.returncode != 0:
+        raise Exception('Abnormal flac termination')
 
 def create_link(rel_dir, file):
     logging.info('Linking %s in %s' % (file, rel_dir))
-    ensure_dir(join(args.cache, rel_dir))
-    os.link(join(args.music, rel_dir, file), join(args.cache, rel_dir, file))
+    ensure_dir(cache_path(rel_dir))
+    os.link(music_path(rel_dir, file), cache_path(rel_dir, file))
 
 for dir, dirs, files in os.walk(args.music):
     assert dir[0:len(args.music)] == args.music
