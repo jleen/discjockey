d83810b34f2809aa97eb27b9034194875635df0d
John Leen <jleen@saturnvalley.org>
2014-04-13 23:38:30 -0700

    Sanitize star

--- djrip.py
+++ djrip.py
@@ -58,6 +58,7 @@ def sanitize_filename(filename):
     filename = filename.replace('/', ' - ')
     filename = filename.replace(':', ' -')
     filename = filename.replace('?', '~')
+    filename = filename.replace('*', '-')
 
     return filename
 
