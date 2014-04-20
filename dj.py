87c9bade36ca5f558b54d9da9ea89bfd76edf52a
John Leen <jleen@saturnvalley.org>
2014-04-20 15:13:13 -0700

    Ident from OS X

--- dj.py
+++ dj.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2012 John Leen
+# Copyright (c) 2012-2014 John Leen
 
 import argparse
 import logging
--- djident.py
+++ djident.py
@@ -1,20 +1,42 @@
+# Copyright (c) 2014 John Leen
+
+import argparse
+import json
+import platform
+import subprocess
+import urllib
+import urllib2
 from xml.etree import ElementTree
-import urllib2, urllib, json, subprocess
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--client')
 parser.add_argument('--user')
 args = parser.parse_args()
 
+
 # Read the CD TOC
 
-p = subprocess.Popen(['/usr/bin/cdrecord', '-toc'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+if platform.system() == 'Darwin':
+    p = subprocess.Popen(['drutil', 'trackinfo'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+else:
+    p = subprocess.Popen(['/usr/bin/cdrecord', '-toc'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 out, err = p.communicate()
 
+
 toc = ''
-for line in out.splitlines():
-    if line.startswith('track'):
-        toc += str(int(line[17:26]) + 150) + ' '
+if platform.system() == 'Darwin':
+    for line in out.splitlines():
+        if line[8:18] == 'trackStart':
+            last_start = int(line[27:]) + 150
+            toc += str(last_start) + ' '
+        elif line[16:25] == 'trackSize':
+            last_length = int(line[27:])
+    # drutil doesn't report the leadout, but Gracenote needs it for ID.
+    toc += str(last_start + last_length) + ' '
+else:
+    for line in out.splitlines():
+        if line.startswith('track'):
+            toc += str(int(line[17:26]) + 150) + ' '
     
 
 # Query Gracenote
