73afccdede373ae3910b38ae09ca310bba98942a
John Leen <jleen@saturnvalley.org>
2015-09-04 21:23:13 -0700

    Update copyrights

--- py/djbeautify.py
+++ py/djbeautify.py
@@ -1,3 +1,5 @@
+# Copyright (c) 2015 John Leen
+
 import re
 import sys
 
--- py/djrip.py
+++ py/djrip.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2013-2014 John Leen
+# Copyright (c) 2013-2015 John Leen
 
 import argparse
 import logging
