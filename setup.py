de9cd7f1bd3a7a96cc63e7e285ee89f018e83234
John Leen <jleen@saturnvalley.org>
2017-05-27 10:26:08 -0700

    CLI for rename

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     entry_points={
         'console_scripts': ['dj = discjockey.transcode:transcode',
                             'frip = discjockey.rip:rip',
+                            'frename = discjockey.rip:rename',
                             'fident = discjockey.ident:ident',
                             'fit = discjockey.fit:fit',
                             'cosmetize = discjockey.beautify:cosmetize',
