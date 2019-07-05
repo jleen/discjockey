9a643c7cd921e3c8261646b637de94dd089bfaae
John Leen <jleen@saturnvalley.org>
2019-07-05 09:58:23 -0400

    Windows can be kept awake too

--- discjockey/sleepless.py
+++ discjockey/sleepless.py
@@ -9,5 +9,11 @@ def sleepless():
     if platform.system() == 'Darwin':
         from discjockey import pmset
         pmset.prevent_idle_sleep('Disc Jockey Rip')
+    elif platform.system() == 'Windows':
+        import ctypes
+        ES_CONTINUOUS = 0x80000000
+        ES_SYSTEM_REQUIRED = 0x00000001
+        ctypes.windll.kernel32.SetThreadExecutionState(
+                ES_CONTINUOUS | ES_SYSTEM_REQUIRED)
 
     subprocess.call(sys.argv[1:])
