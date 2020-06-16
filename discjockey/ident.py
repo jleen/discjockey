cca35427f2e1b68623ebd341f12518ed02cf3dc5
John Leen <jleen@saturnvalley.org>
2020-06-16 09:11:17 -0400

    Use MusicBrainz instead of Gracenote

--- discjockey/ident.py
+++ discjockey/ident.py
@@ -1,5 +1,7 @@
-# Copyright (c) 2014-2016 John Leen
+# Copyright (c) 2014-2020 John Leen
 
+import discid
+import musicbrainzngs as mbz
 import os
 import sys
 import urllib.error
@@ -13,62 +15,39 @@ from xml.etree import ElementTree
 from discjockey import config
 from discjockey import platform as djplatform
 
+mbz.set_useragent("Saturn Valley Disc Jockey", "1.0",
+                  "https://github.com/jleen/discjockey")
 
-def get_tracks_from_gracenote(exit_on_fail):
-    toc = djplatform.read_toc()
 
-    cid = config.gracenote_client
-    uid = config.gracenote_user
+def find_track_list(disc_id, medium_list):
+    for medium in medium_list:
+        for disc in medium['disc-list']:
+            if disc['id'] == disc_id:
+                return medium['track-list']
 
-    url = 'https://c' + cid.split('-')[0] + '.web.cddbp.net/webapi/xml/1.0/'
 
-    root = ElementTree.Element('QUERIES')
+def get_tracks_from_musicbrainz(exit_on_fail):
+    disc_id = discid.read().id
+    disc_data = mbz.get_releases_by_discid(
+            disc_id, includes=['recordings', 'artists'])
 
-    auth = ElementTree.SubElement(root, 'AUTH')
-    client = ElementTree.SubElement(auth, 'CLIENT')
-    user = ElementTree.SubElement(auth, 'USER')
-    client.text = cid
-    user.text = uid
+    # There could be more than one release that includes this disc.  Since
+    # we're just going to traverse back to the disc's representation within the
+    # release, it doesn't really matter which one we choose.  So choose the
+    # first one.
+    release = disc_data['disc']['release-list'][0]
 
-    query = ElementTree.SubElement(root, 'QUERY')
-    query.attrib['CMD'] = 'ALBUM_TOC'
-    mode = ElementTree.SubElement(query, 'MODE')
-    mode.text = 'SINGLE_BEST'  # or SINGLE_BEST_COVER
+    title = release['title']
+    artist = release['artist-credit-phrase']
+    # TODO: Can MusicBrainz give us a genre tag?
 
-    toc_node = ElementTree.SubElement(query, 'TOC')
-    offset = ElementTree.SubElement(toc_node, 'OFFSETS')
-    offset.text = toc
-
-    response_obj = urllib.request.urlopen(url, ElementTree.tostring(root))
-    response_text = response_obj.read()
-    response_tree = ElementTree.fromstring(response_text)
-    response = response_tree.find('RESPONSE')
-
-    status = response.attrib['STATUS']
-    if status != 'OK':
-        if exit_on_fail:
-            print("Gracenote couldn't find that disc.")
-            print()
-            print("Queried for %s" % toc)
-            print("Got response %s" % status)
-            print(response_tree.find('MESSAGE').text)
-            sys.exit(2)
-        else:
-            return []
-
-    album = response.find('ALBUM')
-    artist = urllib.parse.unquote(album.findall('ARTIST')[0].text)
-    title = urllib.parse.unquote(album.findall('TITLE')[0].text)
-    genre = urllib.parse.unquote(album.findall('GENRE')[0].text)
-
-    tracks = []
-    track_tree = album.findall('TRACK')
-    for track in track_tree:
-        tracks.append(urllib.parse.unquote(track.findall('TITLE')[0].text))
+    track_list = find_track_list(disc_id, release['medium-list'])
+    sorted_tracks = sorted(track_list, key=lambda t: int(t['position']))
+    tracks = [track['recording']['title'] for track in sorted_tracks]
 
     lines = []
     if not config.nometa:
-        lines.append('~g %s' % genre)
+        #lines.append('~g %s' % genre)
         lines.append('~r %s' % artist)
         lines.append('~a %s' % title)
         lines.append('')
@@ -102,7 +81,7 @@ def ident(exit_on_fail = True):
             print('--- Insert disc %d of %d ---' % (disc + 1, num_discs))
             djplatform.wait_for_disc()
 
-        lines = get_tracks_from_gracenote(exit_on_fail)
+        lines = get_tracks_from_musicbrainz(exit_on_fail)
 
         if album:
             if not os.path.exists(os.path.dirname(album)):
