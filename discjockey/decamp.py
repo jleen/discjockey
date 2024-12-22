a5156ac9af7e11c09f44841df218bafb49545df0
John Leen <jleen@saturnvalley.org>
2024-12-22 13:18:44 -0500

    Rewrite to handle multi artist albums

--- discjockey/decamp.py
+++ discjockey/decamp.py
@@ -1,46 +1,41 @@
-import os
 import sys
 
+from collections import Counter
+from pathlib import Path
 from zipfile import ZipFile
 
 DELIM = ' - '
 
 
-def bandcamp_prefix(strings):
-    first = strings[0]
-    skip = 0
-    candidate = ''
-    found = ''
-    for i in range(0, first.count(DELIM)):
-        skip = first.find(DELIM, skip) + len(DELIM)
-        candidate = first[0:skip]
-        for s in strings:
-            if s.endswith('.flac') and not s.startswith(candidate):
-                # Me go too far.
-                return found
-        found = candidate
-    return found
-
-
 def decamp():
     with ZipFile(sys.argv[1]) as zipfile:
         files = zipfile.namelist()
-        prefix = bandcamp_prefix(files)
-        try:
-            (artist, album) = prefix[:-len(DELIM)].split(DELIM, 1)
-        except Exception as ex:
-            print("Can't parse common file prefix " + prefix)
-            print("Filenames are:\n" + "\n".join(files))
-            raise ex
-        print('Album is ' + album + ' by ' + artist)
-        where = os.path.join(artist, album)
-        os.makedirs(where, exist_ok=True)
+        tracks = [[f] + f.split(DELIM) for f in files if f.endswith('.flac')]
+        extra = [f for f in files if not f.endswith('.flac')]
+        if len(set([x[2] for x in tracks])) != 1:
+            raise Exception("Multiple album names? I can't even")
 
-        for name in files:
-            new_name = name[len(prefix):] if name.endswith('.flac') else name
-            print('Extracting ' + new_name)
-            dest = os.path.join(where, new_name)
-            if os.path.lexists(dest):
-                raise Exception('Quitting now instead of overwriting')
-            with open(dest, 'wb') as out:
-                out.write(zipfile.read(name))
+        artists = Counter(x[1] for x in tracks)
+        (primary_artist, count) = artists.most_common(1)[0]
+        if count <= len(tracks) / 2:
+            primary_artist = 'Anthology'
+
+        dest_dir = Path(primary_artist, tracks[1][2])
+        dest_dir.mkdir(parents=True)
+
+        for (filename, artist, _, title) in tracks:
+            if artist != primary_artist:
+                dest = f'{Path(title).stem} ({artist}).flac'
+            else:
+                dest = title
+            unzip_to(dest_dir / dest, zipfile, filename)
+
+        for filename in extra:
+            unzip_to(dest_dir / filename, zipfile, filename)
+
+
+def unzip_to(dest, zipfile, content):
+    if Path(dest).exists():
+        raise Exception('Quitting now instead of overwriting')
+    with open(dest, 'wb') as out:
+        out.write(zipfile.read(content))
