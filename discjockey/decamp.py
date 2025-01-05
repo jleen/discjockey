8342930c77aad0b1054f51ad417b9a4168ad4977
John Leen <jleen@saturnvalley.org>
2025-01-04 23:13:28 -0500

    Handle spaced hyphens in album and track names (but not artist names, sigh)

--- discjockey/decamp.py
+++ discjockey/decamp.py
@@ -1,16 +1,31 @@
+import re
 import sys
 
 from collections import Counter
 from pathlib import Path
 from zipfile import ZipFile
 
-DELIM = ' - '
+
+def track_info(filename):
+    parts = filename.split(' - ')
+    artist = parts[0]
+    parts = parts[1:]
+
+    album_parts = []
+    while not re.match('^\\d+ ', parts[0]):
+        album_parts += [parts[0]]
+        parts = parts[1:]
+    album = ' - '.join(album_parts)
+
+    track = ' - '.join(parts)
+
+    return [filename, artist, album, track]
 
 
 def decamp():
     with ZipFile(sys.argv[1]) as zipfile:
         files = zipfile.namelist()
-        tracks = [[f] + f.split(DELIM) for f in files if f.endswith('.flac')]
+        tracks = [track_info(f) for f in files if f.endswith('.flac')]
         extra = [f for f in files if not f.endswith('.flac')]
         if len(set([x[2] for x in tracks])) != 1:
             raise Exception("Multiple album names? I can't even")
