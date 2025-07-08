8d5092b7fa5aa3c735151bd835bc75365a6ba885
John Leen <jleen@saturnvalley.org>
2025-07-07 21:04:11 -0500

    Late import to avoid initializing all dependencies

--- discjockey/dj.py
+++ discjockey/dj.py
@@ -1,18 +1,21 @@
 import sys
 
-from . import beautify, transcode, ident, fit, rip
-
 def main():
     cmd, *sys.argv[1:] = sys.argv[1:]
     if cmd == 'beautify':
+        from . import beautify
         beautify.cosmetize()
     elif cmd == 'transcode':
+        from . import transcode
         transcode.transcode()
     elif cmd == 'ident':
+        from . import ident
         ident.ident()
     elif cmd == 'intake':
+        from . import fit
         fit.fit()
     elif cmd == 'rename':
+        from . import rip
         rip.rename()
     else:
         raise Exception('Unknown command ' + cmd)
